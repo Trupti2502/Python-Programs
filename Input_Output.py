d3d67fc92435a04c9e0530f7b22c6528ee1ca7a0
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-13 23:23:26 +0530

    Input and Output

---
+++ Input_Output.py
@@ -0,0 +1,10 @@
+print("Enter one number")#To display on screen
+no=input()  #To accept from standared input device i.e. keyboard
+
+
+#scanf("%d",&no);  c
+#cin>>no;          c++
+#br.readLine();    java 
+
+print("Your entered no is:",no)
+print("Datatype of entered number is:",type(no))
