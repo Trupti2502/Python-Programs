305fd2f64edcfd0f7cd530df660214fdb8c9e67a
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-13 23:36:23 +0530

    Addition of two numbers by creating function Addition

---
+++ Addition2.py
@@ -0,0 +1,10 @@
+def Addition(value1,value2):
+	ret=value1+value2
+	return ret
+
+no1=int(input("Enter First Number"))
+no2=int(input("Enter Second Number"))
+
+Ans=Addition(no1,no2)
+
+print("Addition is",Ans)
