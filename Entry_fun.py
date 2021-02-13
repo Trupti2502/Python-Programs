d6c0f996bc031b8966d08d0dcb5403827adf2b1b
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-13 23:49:23 +0530

    Calling first function or entry point function

---
+++ Entry_fun.py
@@ -0,0 +1,15 @@
+def Addition(value1,value2):
+ ret=value1+value2
+ return ret
+
+
+def xyz():
+ no1=int(input("Enter first number"))
+ no2=int(input("Enter Second number"))
+ ans=Addition(no1,no2)
+ print("Addition is",ans)
+
+if __name__=="__main__":
+ xyz()
+
+ # 12  13  6  7  8  9  1  2  3  9  10  13
