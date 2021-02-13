b1be6a981a01e86880429359cefdbde409df1395
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-13 23:59:37 +0530

    Use of format method

---
+++ Format_method.py
@@ -0,0 +1,9 @@
+print("Enter First Number")
+no1=int(input())
+
+print("Enter Sirst Number")
+no2=int(input())
+
+Ans=no1+no2
+
+print("Addition of {} and {} is : {}".format(no1,no2,Ans))  #Format method
