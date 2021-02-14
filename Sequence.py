0fa02297f037d8c1106a04d4e257fb950543c604
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-15 00:33:54 +0530

    Program without Iteration and without Selection

---
+++ Sequence.py
@@ -0,0 +1,9 @@
+#Demonstration of sequence
+def fun():
+ print("First line of function")
+ print("Second line of function")
+ 
+
+print("First line of application")
+fun()
+print("Last of application")
