8da2594f0f2c0603585876adcee1ab5e40f0af41
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-14 08:18:55 +0530

    Command Line Arguments In Python

---
+++ Command4.py
@@ -0,0 +1,19 @@
+import sys
+
+def Addition(no1,no2):
+ return no1+no2
+
+def main():
+ if len(sys.argv)<3: 
+  print("INSUFFICIENT ARGUMENTS")
+  return
+
+ if len(sys.argv)>3: 
+  print("INVALID ARGUMENTS")
+  return
+
+ ret=Addition(int(sys.argv[1]),int(sys.argv[2]))
+ print("Addition of{} and {} is:{}".format(sys.argv[1],sys.argv[2],ret))
+
+if __name__=="__main__":
+ main() 	
