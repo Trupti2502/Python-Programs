58d72d709b203489c5c14279a5a8976a9788b904
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-14 07:56:35 +0530

    Defining blank function  or function without containt

---
+++ Blank.py
@@ -0,0 +1,12 @@
+
+
+def mun():
+  pass #pass is the keyword use when we want to only define function but dont want to write anything in function
+
+
+def main():
+  mun()
+ 
+
+if __name__=="__main__":
+  main() 
