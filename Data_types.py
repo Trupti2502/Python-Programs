e6eb1502ee8729008f28513321ed9524a2122686
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-14 08:05:09 +0530

    Some Data Types In Python

---
+++ Data_types.py
@@ -0,0 +1,18 @@
+#SOME DATA TYPES IN PYTHON
+no=11 #integer
+print("value of no is:",no)
+print("datatype of no is",type(no))
+print("unique id of no",id(no)) #printf("%d",&no);
+
+
+value=11.23 #float
+print("value of value is:",value)
+print("datatype of value is",type(value))
+print("unique id of value",id(value))
+
+
+a=4+5j #complex number
+print("value of a is:",a)
+print("datatype of a is",type(a))
+print("unique id of a",id(a))
+
