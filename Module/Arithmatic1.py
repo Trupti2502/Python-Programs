0db4c0d68497fbf85f7f6649572a33f1e90d92f4
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-16 13:06:04 +0530

    contains only definations of fuctions

---
+++ Module/Arithmatic1.py
@@ -0,0 +1,15 @@
+def Addition(a,b):
+ ans=a+b
+ return ans
+
+def Substraction(a,b):
+ ans=a-b
+ return ans
+
+def Multiplication(a,b):
+ ans=a*b
+ return ans
+
+def Division(a,b):
+ ans=a/b
+ return ans
