231c5ff85df281e86e47d990c5a938775199dbbd
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-16 18:00:11 +0530

    some data types in python

--- Data_types.py
+++ Data_types.py
@@ -1,18 +1,39 @@
 #SOME DATA TYPES IN PYTHON
-no=11 #integer
-print("value of no is:",no)
-print("datatype of no is",type(no))
-print("unique id of no",id(no)) #printf("%d",&no);
+a=11#integer
+print("\nvalue of a=:",a)
+print("type of a:",type(a))
 
 
-value=11.23 #float
-print("value of value is:",value)
-print("datatype of value is",type(value))
-print("unique id of value",id(value))
+b=12.45#float
+print("\nvalue of b=:",b)
+print("type of b:",type(b))
+
+c=4+3j#complex
+print("\nvalue of c=:",c)
+print("type of c:",type(c))
 
 
-a=4+5j #complex number
-print("value of a is:",a)
-print("datatype of a is",type(a))
-print("unique id of a",id(a))
+d="Hello"#string
+print("\nvalue of d=:",d)
+print("type of d:",type(d))
 
+
+e=True#boolean
+print("\nvalue of e=:",)
+print("type of e:",type(e))
+
+f=[10,20,30,40,50]#list
+print("\nvalue of d=:",f)
+print("type of c:",type(f))
+
+g=(10,20,30,40)#tuple
+print("\nvalue of g=:",g)
+print("type of g:",type(g))
+
+h={10,20,30,40,}#dictionary
+print("\nvalue of h=:",h)
+print("type of h:",type(h))
+
+i={"a":10,"b":20,"c":30}
+print("\nvalue of i=:",i)
+print("type of i:",type(i))
