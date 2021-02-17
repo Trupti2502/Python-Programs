d628fd3acbabeed2904fde589efa62abcdf66b62
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-17 22:50:58 +0530

    To check given number is Positive/Negative/Zero

---
+++ IsPositive.py
@@ -0,0 +1,15 @@
+
+def IsPositive():
+ print("Enter Number")
+ no=int(input())
+ if no>0:
+  print("Positive Number")
+ elif no<0:
+  print("Negative Number")
+ else:
+  print("Zero")
+
+
+
+if __name__=="__main__":
+ IsPositive()
