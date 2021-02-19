98d8da5b7d24a910574784c4b7f87ad6f2a62cf8
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-19 10:57:59 +0530

    Accept no from user and print pattern

---
+++ Pattern3.py
@@ -0,0 +1,26 @@
+def main():
+ print("Enter Number")
+ no=int(input())
+ for i in range(no):
+  for i in range(no):
+   print(i+1,end="   ")
+  print("\n")
+
+
+
+if __name__=="__main__":
+ main()
+ 
+
+#O/P:-
+"""
+Enter Number
+4
+1   2   3   4   
+
+1   2   3   4   
+
+1   2   3   4   
+
+1   2   3   4   
+"""
