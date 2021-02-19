459b7dfca4830d4f2beb6e5489c114fdc6671c0a
Trupti2502 <78984702+Trupti2502@users.noreply.github.com>
2021-02-19 11:08:36 +0530

    Accept no from user and print pyramid pattern

---
+++ Pattern4.py
@@ -0,0 +1,30 @@
+
+def main():
+ print("Enter Number")
+ no=int(input())
+ 
+ for i in range(1,no+1,1):
+  print("\n")
+  for j in range(1,i+1,1):
+   print(j,end=" ")
+
+
+if __name__=="__main__":
+ main()
+
+
+#O/P:-
+"""
+Enter Number
+4
+
+
+1 
+
+1 2 
+
+1 2 3 
+
+1 2 3 4 user@user-Lenovo-G50-80:~/Desktop/PYTHON/Assignment/Assignment2$ 
+
+"""
